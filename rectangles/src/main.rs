1705a4b9911bce54b90832a12f24f6f150ac47e2
Hugo Perez <hugopf.mail@gmail.com>
2024-04-08 13:58:41 +0200

    Update Exercise-02: Rectangles to use struct methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,18 +4,34 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("The rectangle is: {:?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    )
-}
+    let rect2 = Rectangle::square(10);
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
