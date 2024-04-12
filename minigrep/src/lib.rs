62ac961ed5ac5fc918c11d42e2f8a68db282ecbb
Hugo Perez <hugopf.mail@gmail.com>
2024-04-12 12:47:31 +0200

    Update Exercise-03 'Minigrep' to use iters

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,28 +17,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[derive(Debug)]
@@ -51,13 +40,16 @@ pub struct Config {
 impl<'a> Config {
     const ENV_IGNORE_CASE: &'a str = "IGNORE_CASE";
 
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // First arg ignored (bin name)
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("did not get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("did not get a file_path string"),
+        };
 
         let ignore_case = env::var(Self::ENV_IGNORE_CASE).is_ok();
 
